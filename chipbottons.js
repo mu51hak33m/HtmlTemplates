ff771f4145c7b7da1ff5a02d28d19e0c174c077b
mohammed <musthakeemm5@gmail.com>
2024-06-23 21:14:46 +0300

    Adding multiple items in one commit

--- chipbottons.js
+++ chipbottons.js
@@ -1,8 +1,16 @@
+const ativated = false
 document.querySelectorAll('.chip').forEach(chip=>{
     chip.addEventListener('click',()=>{
         const background = chip.querySelector('.chip-background');
+        if(ativated){
+            background.classList.toggle('deactivate');
+            ativated = false
+        }
         if(background){
             background.classList.toggle('active');
+            ativated = true
+        
         }
+
     });
 });
