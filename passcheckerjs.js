2e9fe4193620195231bafe1e5d52ecf43c32683a
mohammed <musthakeemm5@gmail.com>
2024-09-08 19:53:36 +0300

    Adding the password checker JS files to the mix along with some changes on animate.css file as well, not sure what the changes are

---
+++ passcheckerjs.js
@@ -0,0 +1,41 @@
+document.getElementById("password").addEventListener("input",function(){
+const password = this.value;
+
+const upperCasePattern = /[A-Z]/;
+const lowerCasePattern = /[a-z]/;
+const numberPattern = /[0-9]/;
+const specialCharPattern = /[!@#$%^&*()_+{}<>|]/;
+
+updateValidation(
+    "upperCase",upperCasePattern.test(password)
+);
+updateValidation(
+    "lowerCase",lowerCasePattern.test(password)
+);
+updateValidation(
+    "number", numberPattern.test(password)
+
+);
+updateValidation(
+    "specialChar",specialCharPattern.test(password)
+);
+});
+
+function updateValidation(elementID, isValid){
+    const element = document.getElementById(elementID);
+    const icon = element.querySelector("i");
+
+    if(isValid){
+        element.classList.remove("invalid");
+        element.classList.add("valid");
+        icon.classList.remove("bi-shield-x");
+        icon.classList.add("bi-shield-check");
+    }
+    else{
+        element.classList.remove("valid");
+        element.classList.add("invalid");
+        icon.classList.remove("bi-shield-check");
+        icon.classList.add("bi-shield-x");
+        
+    }
+}
